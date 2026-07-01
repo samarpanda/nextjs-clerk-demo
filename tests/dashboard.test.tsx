6c98a390962f4f9fe99c076184f7facf15a35fc1
Samar Panda <er.samarpanda@gmail.com>
2026-07-01 11:05:54 +0530

    feat: vitest setup to test nextjs page with test coverage

---
+++ tests/dashboard.test.tsx
@@ -0,0 +1,32 @@
+import { vi } from "vitest";
+import HomePage from "../src/app/page";
+import { redirect } from "next/navigation";
+
+vi.mock("next/navigation", () => ({
+  redirect: vi.fn(),
+}));
+
+vi.mock("@clerk/nextjs/server", () => {
+  const mockedFunctions = {
+    auth: () =>
+      new Promise((resolve) =>
+        resolve({ userId: "user_2NNNEWasdfasdfsfadsC" }),
+      ),
+    ClerkProvider: ({ children }: { children: React.ReactNode }) => (
+      <div>{children}</div>
+    ),
+    useUser: () => ({
+      isSignedIn: true,
+      user: {
+        id: "user_2NNNEWasdfasdfsfadsC",
+        fullName: "Samar Panda",
+      },
+    }),
+  };
+
+  return mockedFunctions;
+});
+test("redirects signed-in user", async () => {
+  await HomePage();
+  expect(redirect).toHaveBeenCalledWith("/dashboard");
+});
